af771da4a74f0c35d446e6fb9256d4d1a611351f
Danika <danikaphann@gmail.com>
2025-03-25 12:22:52 +0700

    nika push

---
+++ one.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+echo "Enter the path you want to check (relative to current directory): "
+read userInput
+echo $userInput >> console.log
+final=$(eval echo $userInput)
+#could use if else but need to check a lot
+ls -l $final
