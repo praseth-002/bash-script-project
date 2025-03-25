d9dfb2d917e5527f2c903cbc8320698e48e7c81d
Pich <bopich568317@gmail.com>
2025-03-25 12:28:52 +0700

    pish push

---
+++ two.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+echo "Enter the file or directory you want to make a backup: "
+read userFile
+echo $userFile >> console.log
+final1=$(eval echo $userFile)
+echo "Enter the location of your backup: "
+read userLocation
+echo $userLocation >> console.log
+final2=$(eval echo $userLocation)
+cp -r $final1 $final2
