47eed3417e7ccc4c314566da3494564cce698597
praseth-002 <prasethsokha@gmail.com>
2025-03-31 12:15:37 +0700

    some final fixes

--- five.sh
+++ five.sh
@@ -4,4 +4,4 @@ echo $location >> console.log
 expandPath=$(eval echo $location)
 read -p "Enter what you want to search for: " search
 echo $search >> console.log
-ls $expandPath | grep "$search"
+ls "$expandPath" | grep "$search"
