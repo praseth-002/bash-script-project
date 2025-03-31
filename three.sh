4d4fc0ae8f551dc44494b3aa44f9c14bae20f607
praseth-002 <prasethsokha@gmail.com>
2025-03-31 12:13:45 +0700

    some final fixes

--- three.sh
+++ three.sh
@@ -2,5 +2,5 @@
 read -p "Enter the directory you want to check the amount of files: " location
 echo $location >> console.log
 expandPath=$(eval echo $location)
-result=`ls -l $expandPath | wc -l`
+result=`ls "$expandPath" | wc -l`
 echo "There are" $result "items in the $location path.";
