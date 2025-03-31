5d85329085c53972756d85e3b3815433ac8258ca
praseth-002 <prasethsokha@gmail.com>
2025-03-31 12:14:38 +0700

    some final fixes

--- four.sh
+++ four.sh
@@ -2,4 +2,4 @@
 read -p "Enter the path you want to check storage usage: " location
 echo $location >> console.log
 expandPath=$(eval echo $location)
-du -sh 2>/dev/null $expandPath
+du -sh 2>/dev/null "$expandPath"
