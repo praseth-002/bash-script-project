85ec2ecc732bcad3b8d71385d12ca5cb9b760eb6
Phon Sokleaphea <phonsokleaphea@Phons-MacBook-Pro.local>
2025-04-02 22:51:12 +0700

    push again

--- three.sh
+++ three.sh
@@ -3,4 +3,5 @@ read -p "Enter the directory you want to check the amount of files: " location
 echo $location >> console.log
 expandPath=$(eval echo $location)
 result=`ls "$expandPath" | wc -l`
-echo "There are" $result "items in the $location path.";
+echo "There are" $result "items in the $location path.";  
+
